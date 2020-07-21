d0674fffe1c29e80808a0c510fbf1f447e83a4a1
Reznitchenko <inna.reznitchenko@accenture.com>
2020-07-21 10:12:12 -0400

    environment file added

--- project1-ui/src/remote/users-api/index.ts
+++ project1-ui/src/remote/users-api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { lbBaseUrl } from '../../environment'
 
 export const userClient = axios.create({
-    baseURL:'http://localhost:2006',
+    baseURL:lbBaseUrl,
     headers:{
         'Content-Type': 'application/json'
     },
